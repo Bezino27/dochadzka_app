73ad227604720a635f5d69afb75c5d9e29f2069f
kurri <tomikbez@gmail.com>
2025-02-26 16:29:14 +0100

    Aktualizácia URL

--- dochadzka_frontend/src/components/trainings.js
+++ dochadzka_frontend/src/components/trainings.js
@@ -19,25 +19,33 @@ const Trainings = () => {
 
 
     return (
-        <div>
-            <h1>Tréningy</h1>
-            <ul>
+        <div className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+            <h1 className="text-2xl font-bold text-gray-800 mb-4">Tréningy</h1>
+            <ul className="space-y-3">
                 {trainings.map((training) => {
                     // Naformátovanie dátumu na "deň mesiac" v slovenčine
-                    const formattedDate = new Intl.DateTimeFormat('sk-SK', {
-                    day: '2-digit',
-                    month: 'long',
-                }).format(new Date(training.date));
+                    const formattedDate = new Intl.DateTimeFormat("sk-SK", {
+                        day: "2-digit",
+                        month: "long",
+                    }).format(new Date(training.date));
                     const categoryName = categories[training.category] || "Neznáma kategória";
 
                     return (
-                    <li key={training.id}>
-                {training.day} {formattedDate} - {categoryName} ---> {training.players.length} hráčov
-            </li>
-            );
-            })}
-        </ul>
-</div>
+                        <li
+                            key={training.id}
+                            className="flex justify-between items-center p-4 bg-gray-100 border-l-4 border-blue-500 rounded-lg"
+                        >
+          <span className="text-lg font-semibold text-gray-700">
+            {training.day} {formattedDate} - {categoryName}
+          </span>
+                            <span className="text-sm bg-blue-500 text-white px-3 py-1 rounded-full">
+            {training.players.length} hráčov
+          </span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
 );
 };
 
