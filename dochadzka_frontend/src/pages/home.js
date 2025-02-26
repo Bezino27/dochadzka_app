27273c2b2ae5c984207c42e88d868ca0e5ca61ce
kurri <tomikbez@gmail.com>
2025-02-26 16:24:45 +0100

    Aktualizácia URL

--- dochadzka_frontend/src/pages/home.js
+++ dochadzka_frontend/src/pages/home.js
@@ -3,10 +3,9 @@ import React from "react";
 const Home = () => {
     return (
         <>
-            <h1>Vitaj v aplikácii na správu tréningov</h1>
-            <div className="bg-blue-500 text-white p-4">
-                TailwindCSS funguje!
-            </div>
+            <h1>
+                Vitajte na správe dochádzky klubu ATU košice
+            </h1>
         </>
     );
 };
